466fe1223f75d222da012d298f5a25ec112c269e
95279999 <lix9527@outlook.com>
2025-09-01 22:20:01 +0800

    添加文件系统功能

--- VSSystem/Private/VSFile.cpp
+++ VSSystem/Private/VSFile.cpp
@@ -22,3 +22,51 @@ VSFile::~VSFile() {
 bool VSFile::Flush() {
     return(fflush(m_pFileHandle) == 0);
 }
+
+bool VSFile::Seek(unsigned int uiOffset, unsigned int uiOrigin) {
+    VSMAC_ASSERT(m_pFileHandle);
+    fseek(m_pFileHandle,uiOffset,uiOrigin);
+    return true;
+}
+
+bool VSFile::Open(const TCHAR *pFileName, unsigned int uiOpenMode) {
+    if (m_pFileHandle)
+    {
+        fclose(m_pFileHandle);
+    }
+    VSMAC_ASSERT(!m_pFileHandle);
+    VSMAC_ASSERT(uiOpenMode < OM_MAX);
+    unsigned int uiLen = VSStrLen(pFileName);
+    if (uiLen < VSMAX_PATH - 1)
+    {
+        if(!VSMemcpy(m_tcFileName,pFileName,uiLen + 1))
+            return false;
+    }
+    else
+    {
+        return false;
+    }
+
+    m_uiOpenMode = uiOpenMode;
+    if (m_uiOpenMode == OM_RB || m_uiOpenMode == OM_RT)
+    {
+        struct _stat64i32 kStat;
+        if (_tstat(pFileName,&kStat) != 0)
+        {
+            return false;
+        }
+        m_uiFileSize = kStat.st_size;
+    }
+
+    errno_t uiError = _tfopen_s(&m_pFileHandle,pFileName,ms_cOpenMode[m_uiOpenMode]);
+    if (uiError)
+    {
+        return 0;
+    }
+    if (!m_pFileHandle)
+    {
+        return 0;
+    }
+
+    return true;
+}
